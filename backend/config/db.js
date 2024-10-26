a659ae4ae86a90d46f37beab259b66ffc799c28c
Mukulsh26 <mukulsh7496@gmail.com>
2024-10-26 19:07:01 +0530

    feat: fixed error

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,23 +2,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+// Initialize Sequelize with MySQL settings
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-  }
-);
+    dialect: 'mysql', // Ensure this is set to 'mysql'
+    dialectModule: require('mysql2'), // This line is optional but can be added for clarity
+});
 
+// Test the connection
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connected...');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Database connected...');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
